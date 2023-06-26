5c63114a3a21a08f47bc146332c043ae0d205785
MarioBasilio08 <andres.basilio@outlook.es>
2023-06-26 14:23:10 -0600

    v1

---
+++ Example_Remoting/Cliente/cliente.cs
@@ -0,0 +1,33 @@
+﻿using Servidor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    public class OrderClient
+    {
+        public static void Main()
+        {
+            ChannelServices.RegisterChannel(new TcpChannel());
+
+            Pedido remote = (Pedido)Activator.GetObject(
+                                  typeof(Pedido), //Tipo de objeto remoto
+                                  "tcp://localhost:2255/pedidos.rem"); //URL del objeto remoto
+
+            Console.Write("Cliente: ");
+            string cli = Console.ReadLine();
+            Console.Write("Producto: ");
+            int pro = Int32.Parse(Console.ReadLine());
+            Console.Write("Cantidad: ");
+            int can = Int32.Parse(Console.ReadLine());
+            remote.HazPedido(cli, pro, can);
+            Console.WriteLine("Pedido Realizado");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example_Remoting/Servidor/servidor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace Servidor
+{
+    public class Pedido : MarshalByRefObject
+    {
+        public void HazPedido(string cli, int pro, int can)
+        {
+            DateTime dt = DateTime.Now;
+            string today = dt.ToString("dd-MMM-yyyy");
+            string record = today + "|" + cli + "|" + pro + "|" + can;
+            StreamWriter sw = new StreamWriter("\\pedidos.txt", true);
+            sw.WriteLine(record);
+            sw.Close();
+        }
+
+    }
+
+
+
+    public class PedidoServidor
+    {
+        public static void Start()
+        {
+            ChannelServices.RegisterChannel(new TcpChannel(2255));
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                                    typeof(Pedido),
+                                    "pedidos.rem",
+                                    WellKnownObjectMode.SingleCall);
+        }
+        public static void Main()
+        {
+            Start();
+            Console.WriteLine("pedido.rem Servicio Iniciado, pulse una tecla para terminar");
+            Console.ReadLine();
+        }
+    }
+}
